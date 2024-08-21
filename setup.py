46d1a9eea24a292cefadb39b8823de6826dc7c22
tivanai2020 <tivanai2020@gmail.com>
2024-08-21 14:50:54 +0330

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mylib',
+    version='0.1',
+    packages=find_packages(),
+    description='',
+    author='tivan',
+    author_email='tivanai2020@gmail.com',
+    url='https://github.com/tivanai2020/tivan',  # Update with your actual URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: ',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
